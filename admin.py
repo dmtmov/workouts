dc2d0d509d6ce43c378c3d2610f7c176bcc5ea3d
d <d@art.local>
2023-01-19 23:03:51 +0200

    add ignores; remove old files

--- admin.py
+++
@@ -1,8 +0,0 @@
-from sqladmin import ModelAdmin
-from models import UserModel
-
-
-class UserAdmin(ModelAdmin, model=UserModel):
-    column_list = ["id", "name"]
-
-
--- database.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-
-
-engine = create_engine(
-    "sqlite:///sqlite.db",
-    connect_args={"check_same_thread": False},
-)
-
-
